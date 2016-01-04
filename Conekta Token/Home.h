5ed338323f7dac6fb3e511a672b53666b160842e
g1sus <proyectos_technology@hotmail.com>
2016-01-04 14:25:05 -0600

    ConektaToken

---
+++ Conekta Token/Home.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Conekta Token
+//
+//  Created by Jesus Hernandez on 28/11/15.
+//  Copyright © 2015 Jesus Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Home : UIViewController
+
+
+@end
+
---
+++ Conekta Token/Home.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  Conekta Token
+//
+//  Created by Jesus Hernandez on 28/11/15.
+//  Copyright © 2015 Jesus Hernandez. All rights reserved.
+//
+
+#import "Home.h"
+#import "Conekta.h"
+
+@interface Home ()
+
+@end
+
+@implementation Home
+
+- (void)viewDidLoad {
+    // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) getToken
+{
+    Conekta *conekta = [[Conekta alloc] init];
+    [conekta setDelegate: self];
+    [conekta setPublicKey:@"your_public_key"];
+    [conekta collectDevice];
+    
+    
+    // create objecti type conekta
+    Card *card = [conekta.Card initWithNumber: @"credit_card_number" name: @"owner_credit_card" cvc: @"000" expMonth: @"01" expYear: @"2020"];
+    
+    Token *token = [conekta.Token initWithCard:card];
+    
+    [token createWithSuccess: ^(NSDictionary *data) {
+        NSLog(@"%@", data);
+    } andError: ^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+}
+
+@end
